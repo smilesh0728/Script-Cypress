208263122799d61f4f2ff604b1e0238adcbc100d
SIA <choi.jin.hwan@gmail.com>
2021-04-27 18:32:46 +0900

    selenium을 활용한 로그인성공

--- example/test.js
+++ example/test.js
@@ -1,21 +1,30 @@
 require('chromedriver');
-const webdriver = require('selenium-webdriver');
+//const webdriver = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-let driver = new webdriver.Builder()
-  .forBrowser('chrome')
-  //.usingServer('http://localhost:4444/wd/hub')
-  .build();
+
+const driver = new Builder().forBrowser('chrome').build();
+// let driver = new webdriver.Builder()
+
+//   .forBrowser('chrome')
+//   //.usingServer('http://localhost:4444/wd/hub')
+//   .build();
 
 driver.get('http://business.qa.fastcampus.co.kr');
 
-var emailname = "jinhwan.choi@fastcampus.co.kr";
+var usr = "jinhwan.choi@fastcampus.co.kr";
 var pw = "test1234";
 
-var email = driver.findElement(webdriver.By.name('email'));
-var passward = driver.findElement(webdriver.By.name('password'));
-var submitBtn = driver.findElement(webdriver.By.className('btn btn--md btn--wide'));
+//EmailUser
+//driver.findElement(webdriver.By.name('email')).sendKeys(usr);
+driver.findElement(By.name('email')).sendKeys(usr);
 
-email.sendKeys(emailname);
-passward.sendKeys(pw);
+//Password 
+//driver.findElement(webdriver.By.name('password')).sendKeys(pw, webdriver.Key.ENTER);
+driver.findElement(By.name('password')).sendKeys(pw, Key.ENTER);
 
-submitBtn.submit();
+//const {Builder, By, Key, until} = require('selenium-webdriver');를 추가한 후 click 동작함.
+//driver.findElement(By.xpath("//span[contains(@class,'primary-post__more')]")).click();
+
+driver.sleep(20000);
+//driver.quit();
