42cfa70a19db20eb358cc870783791151be3e862
SIA <choi.jin.hwan@gmail.com>
2021-05-12 13:29:54 +0900

    Viewport적용

--- cypress/integration/loginColoso.spec.js
+++ cypress/integration/loginColoso.spec.js
@@ -1,6 +1,6 @@
 describe('로그인 테스트',function(){
 
-    it ('정상적인 아이디로 로그인', function() {
+    it ('가입하지 않은 아이디로 로그인', function() {
         // 사이트 접속
         // 환경과 서비스명을 이용해서 사이트 접속하도록 구현 필요
 
@@ -17,17 +17,23 @@ describe('로그인 테스트',function(){
 
         // 이메일을 입력한다. 
         // DB에서 가져오는 구조로 구현 필요
-        cy.get('input[name=email]')
+
+        // 10번 중에 2번 정도 에러 나서 wait 추가함...
+        cy.wait(1000)
+        
+        cy.contains('이메일').click()
+        cy.get('#user-email')
         .type('jinhwan.choi@fastcampus.co.kr')
         
         // 비밀번호를 입력한다.
         // DB에서 가져오는 구조로 구현 필요
-        cy.get('input[name=password]')
+        cy.contains('비밀번호').click()
+        cy.get('#user-password')
         .type('test1234')
 
         //로그인 버튼을 누른다.
         cy.get('.btn').click()
-        cy.get('.nav-menu__links').should('be.visible') 
+        cy.contains('인증 에러').should('be.visible') 
     
 
     
--- cypress/integration/loginFastCampus.spec.js
+++ cypress/integration/loginFastCampus.spec.js
@@ -22,9 +22,11 @@ describe('로그인 테스트',function(){
             cy.root().submit()
 
         })
+        //기대결과
         cy.contains('로그인 정보가 일치하지 않습니다').should('be.visible') 
 
     })
+    
     it ('정상적인 아이디로 로그인', function() {
         // 사이트 접속
         // 환경과 서비스명을 이용해서 사이트 접속하도록 구현 필요
@@ -50,6 +52,7 @@ describe('로그인 테스트',function(){
         cy.get('.btn').click()
 
         //홈화면으로 이동하였는지 확인
+        //기대결과
         cy.get('.em').contains('최진환').should('be.visible') 
     
     })
