0710eb2610ed3a9f34f3b00251d08a90240a7c49
SIA <choi.jin.hwan@gmail.com>
2021-05-13 18:44:33 +0900

    불필요한 코드 삭제

--- cypress/integration/paymenet.spec.js
+++ cypress/integration/paymenet.spec.js
@@ -1,46 +1,43 @@
-describe('결제 테스트',function(){
+describe('결제 테스트', function () {
 
-    //로그인한다.
-    it ('로그인하기', function() {
-        // 사이트 접속
-        // 환경과 서비스명을 이용해서 사이트 접속하도록 구현 필요
-
-        cy.visit('/')
-
-        //로그인 페이지로 이동하는 PATH
-        cy.contains('로그인').click()
-        cy.contains('이메일로 시작하기').click()
-
-        // 이메일을 입력한다. 
-        // DB에서 가져오는 구조로 구현 필요
-        cy.get('input[name=email]')
-        .type('jinhwan.choi@fastcampus.co.kr',{delay:100})
-        .should('have.value','jinhwan.choi@fastcampus.co.kr')
-        
-        // 비밀번호를 입력한다.
-        // DB에서 가져오는 구조로 구현 필요
-        cy.get('input[name=password]')
-        .type('test1234')
-
-        //로그인 버튼을 누른다.
-        cy.get('.btn').click()
-
-        //홈화면으로 이동하였는지 확인
-        //기대결과
-        cy.get('.em').contains('최진환').should('be.visible') 
-    
-    })
     //마케팅 브랜치 페이지로 이동한다.
-    it ('마케팅 페이지로 이동하기', function() {
+    it('상품페이지로 이동', function () {
+        //Staging의 상품페이지로 이동 -> qa에는 없는 경우가 있음
+        //TODO
+        //상품 페이지를 테스트 데이터로 생성 필요
+        //
+        //TODO
+        //GNB로 들어가는 방법을 현재 찾지 못해 직접 상품페이지로 이동했음.
+        //
+        cy.visit('https://staging.fastcampus.co.kr/mktg_online_digitmktg')
 
         //브랜치 페이지로 이동
-        cy.get('.nav-menu')
-        .contains('카테고리', {force: true})
-        .click()
-        cy.contains('마케팅').click()
-
- 
-    
+        //수강신청 누르기
     })
+    it('수강신청 누르기', function () {
+        cy.get('.vn-purchase__btn').click()
+        cy.url().should('include', 'auth')
+    })
+
+    it('로그인하기', function () {
+        cy.contains('이메일로 시작하기').click()
+
+        // 아아디 & 비밀번호
+        //테스트 데이터에서 가져오도록 구현 필요
+        //유지보수를 위해서는 별도 모듈에서 관리 필요 
+        cy.get('form').within(($form) => {
+            cy.get('input[name=email]')
+                .type('jinhwan.choi@fastcampus.co.kr', { delay: 100 })
+                .should('have.value', 'jinhwan.choi@fastcampus.co.kr')
+
+            cy.get('input[name=password]')
+                .type('test1234')
+
+            cy.root().submit()
+
+        })
+
+    })
+
 
 })
