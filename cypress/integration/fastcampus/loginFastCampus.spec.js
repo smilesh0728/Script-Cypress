73dd43f4223a4ddabe6c12b7e8fa2e00cb7f44ee
SIA <choi.jin.hwan@gmail.com>
2021-05-14 17:09:30 +0900

    수강신청취소 기능 추가

--- cypress/integration/fastcampus/loginFastCampus.spec.js
+++ cypress/integration/fastcampus/loginFastCampus.spec.js
@@ -7,6 +7,7 @@ describe('viewport 테스트', () => {
         it('비정상 아이디로 로그인', function () {
             // 사이트 접속
             // 환경과 서비스명를 입력받아서 해당 사이트 접속하도록 구현 필요
+            cy.viewport(1280, 720)
             cy.visit('/')
 
             //로그인 페이지로 이동하는 PATH
@@ -31,6 +32,35 @@ describe('viewport 테스트', () => {
             cy.contains('로그인 정보가 일치하지 않습니다').should('be.visible')
 
         })
+        it ('정상적인 아이디로 로그인', function() {
+            // 사이트 접속
+            // 환경과 서비스명을 이용해서 사이트 접속하도록 구현 필요
+    
+            cy.visit('/')
+    
+            //로그인 페이지로 이동하는 PATH
+            cy.contains('로그인').click()
+            cy.contains('이메일로 시작하기').click()
+    
+            // 이메일을 입력한다. 
+            // DB에서 가져오는 구조로 구현 필요
+            cy.get('input[name=email]')
+            .type('jinhwan.choi@fastcampus.co.kr',{delay:100})
+            .should('have.value','jinhwan.choi@fastcampus.co.kr')
+    
+            // 비밀번호를 입력한다.
+            // DB에서 가져오는 구조로 구현 필요
+            cy.get('input[name=password]')
+            .type('test1234')
+    
+            //로그인 버튼을 누른다.
+            cy.get('.btn').click()
+    
+            //홈화면으로 이동하였는지 확인
+            //기대결과
+            cy.get('.em').contains('최진환').should('be.visible') 
+    
+        })
     })
     context('iphone-5 resolution', () => {
         beforeEach(() => {
@@ -39,7 +69,7 @@ describe('viewport 테스트', () => {
 
         it('비정상 아이디로 로그인', function () {
             // 사이트 접속
-            cy.visit('https://qa.fastcampus.co.kr')
+            cy.visit('/')
 
             //로그인 페이지로 이동하는 PATH
             cy.get('.nav-opener > svg').click()
--- cypress/integration/fastcampus/paymenet.spec.js
+++ cypress/integration/fastcampus/paymenet.spec.js
@@ -9,7 +9,7 @@ describe('결제 테스트', function () {
         //TODO
         //GNB로 들어가는 방법을 현재 찾지 못해 직접 상품페이지로 이동했음.
         //
-        cy.visit('https://staging.fastcampus.co.kr/mktg_online_digitmktg')
+        cy.visit('https://qa.fastcampus.co.kr/edu_career_mktg')
 
         //브랜치 페이지로 이동
         //수강신청 누르기
@@ -39,5 +39,42 @@ describe('결제 테스트', function () {
 
     })
 
+    //API로 수강신청상태인지 확인해서 수강신청 상태이면 분기를 통해 스킵되어야 함..
+    //URL을 가져와서 Product ID를 알아야 함..
+    it('수강신청서 제출 누르기', function () {
+        cy.wait(3000)
+        cy.get('.btn__flex').contains('수강신청서 제출').click({force: true})
+    })
+    it('수강신청취소하기 ', function() {
+        cy.visit('/')
+
+        //로그인 페이지로 이동하는 PATH
+        cy.contains('로그인').click()
+        cy.contains('이메일로 시작하기').click()
+
+        // 이메일을 입력한다. 
+        // DB에서 가져오는 구조로 구현 필요
+        cy.get('input[name=email]')
+            .type('jinhwan.choi@fastcampus.co.kr', { delay: 100 })
+            .should('have.value', 'jinhwan.choi@fastcampus.co.kr')
+
+        // 비밀번호를 입력한다.
+        // DB에서 가져오는 구조로 구현 필요
+        cy.get('input[name=password]')
+            .type('test1234')
+
+        //로그인 버튼을 누른다.
+        cy.get('.btn').click()
+
+        //홈화면으로 이동하였는지 확인
+        //기대결과
+        cy.get('.em').contains('최진환').should('be.visible')
+        cy.contains('배우면 바뀐다').click()
+        cy.get(':nth-child(2) > .nav-utils__a').click()
+        
+        //수강신청 취소하기 선택
+        cy.get('.vn-enrollment__payment__func > .vn-mypage__more').click({force: true})
+        cy.get('.btn-group > .btn').click()
+    })
 
 })
